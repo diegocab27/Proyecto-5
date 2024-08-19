24182260872414e3a2b18b55751bfaaf95cd894c
diegocab27 <diego.cabrera.c@hotmail.com>
2024-08-19 19:59:28 -0400

    agregando error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
+import Header from './components/Header.jsx'
+import App from './components/App.jsx'
 import './index.css'
-import Header from './Header.jsx'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <Header/>
+    <ErrorBoundary>
     <App/>
+    </ErrorBoundary>
   </React.StrictMode>,
 )
